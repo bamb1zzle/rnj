00781aa735e10cfe35242e1822d4163c4b38534f
bamb1 <110364583+bamb1zzle@users.noreply.github.com>
2023-07-11 16:13:21 +1200

    firebasedelete

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDYzpjj8m0_8jwPFUZC5fXLstMncHVPNF8",
-  authDomain: "readinglist-f080d.firebaseapp.com",
-  projectId: "readinglist-f080d",
-  storageBucket: "readinglist-f080d.appspot.com",
-  messagingSenderId: "737756515150",
-  appId: "1:737756515150:web:a0f84a6ebce7d15fd96c1f",
-  measurementId: "G-MEPGC0RL3T"
+
 };
 
 // Initialize Firebase
